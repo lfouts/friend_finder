dd79f68594f89e6b4ed7cb0c3331a3b1ce96b3e8
lfouts <lorenfouts@yahoo.com>
2018-08-21 15:56:57 -0400

    all new files

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+
+	  {
+	    "name": "Ahmed",
+	    "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
+	    "scores":[
+	        5,
+	      	1,
+	      	4,
+	      	2,
+	      	5,
+	      	2,
+	      	2,
+	      	5,
+	      	3,
+	      	1
+	    ]
+	  },
+	  {
+	    "name": "Billy",
+	    "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
+	    "scores":[
+	        3,
+	        3,
+	        2,
+	        4,
+	        1,
+	        2,
+	        5,
+	        1,
+	        3,
+	        4
+	    ]
+	  },
+	  {
+	    "name": "Laura",
+	    "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
+	    "scores":[
+	        1,
+	        2,
+	        1,
+	        2,
+	        1,
+	        3,
+	        1,
+	        1,
+	        4,
+	        1
+	    ]
+	  }
+	];
+
+
+module.exports = friends;
